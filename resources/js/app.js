a6996b68b946023a6a2a79a3a64f67e8fe0c44a9
rejohnbd <rejohn.cse@gmail.com>
2022-06-29 16:35:02 +0600

    Add Project Route, basic component

--- resources/js/app.js
+++ resources/js/app.js
@@ -6,10 +6,15 @@ import VueRouter from "vue-router";
 
 Vue.use(VueRouter);
 
+// Route Imported
+import { routes } from "./routes";
+
 const router = new VueRouter({
     routes,
+    mode: "history",
 });
 
 const app = new Vue({
+    router,
     el: "#app",
 });
