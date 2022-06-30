c70fe79fe4972d65def700967125102beb61a0a7
rejohnbd <rejohn.cse@gmail.com>
2022-06-30 17:28:43 +0600

    Add Employee Create Component

--- resources/js/routes.js
+++ resources/js/routes.js
@@ -2,7 +2,10 @@ let login = require("./components/auth/login.vue").default;
 let register = require("./components/auth/register.vue").default;
 let forget = require("./components/auth/forget.vue").default;
 let logout = require("./components/auth/logout.vue").default;
+// End Authentication
 let home = require("./components/home.vue").default;
+// Employee
+let storeemployee = require("./components/employee/create.vue").default;
 
 export const routes = [
     { path: "/", component: login, name: "/" },
@@ -10,4 +13,10 @@ export const routes = [
     { path: "/forget", component: forget, name: "forget" },
     { path: "/logout", component: logout, name: "logout" },
     { path: "/home", component: home, name: "home" },
+
+    {
+        path: "/store-employee",
+        component: storeemployee,
+        name: "store-employee",
+    },
 ];
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -21,36 +21,44 @@
             <!-- Sidebar -->
             <div id="sidebar" v-show="$route.path === '/' || $route.path === '/register' || $route.path === '/forget' ? false : true " style="display: none;">
                 <ul class="navbar-nav sidebar sidebar-light accordion" id="accordionSidebar">
-                    <a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.html">
+                    <router-link class="sidebar-brand d-flex align-items-center justify-content-center" to="/home">
                         <div class="sidebar-brand-icon">
                             <img src="{{ asset('backend/img/logo/logo2.png') }}">
                         </div>
                         <div class="sidebar-brand-text mx-3">RuangAdmin</div>
-                    </a>
+                    </router-link>
                     <hr class="sidebar-divider my-0">
                     <li class="nav-item active">
-                        <a class="nav-link" href="index.html">
+                        <router-link class="nav-link" href="/home">
                             <i class="fas fa-fw fa-tachometer-alt"></i>
-                            <span>Dashboard</span></a>
+                            <span>Dashboard</span>
+                        </router-link>
                     </li>
                     <hr class="sidebar-divider">
                     <div class="sidebar-heading">
                         Features
                     </div>
                     <li class="nav-item">
-                        <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseBootstrap" aria-expanded="true" aria-controls="collapseBootstrap">
+                        <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseEmployeeBootstrap" aria-expanded="true" aria-controls="collapseBootstrap">
                             <i class="far fa-fw fa-window-maximize"></i>
-                            <span>Bootstrap UI</span>
+                            <span>Employee</span>
                         </a>
-                        <div id="collapseBootstrap" class="collapse" aria-labelledby="headingBootstrap" data-parent="#accordionSidebar">
+                        <div id="collapseEmployeeBootstrap" class="collapse" aria-labelledby="headingBootstrap" data-parent="#accordionSidebar">
                             <div class="bg-white py-2 collapse-inner rounded">
-                                <h6 class="collapse-header">Bootstrap UI</h6>
-                                <a class="collapse-item" href="alerts.html">Alerts</a>
-                                <a class="collapse-item" href="buttons.html">Buttons</a>
-                                <a class="collapse-item" href="dropdowns.html">Dropdowns</a>
-                                <a class="collapse-item" href="modals.html">Modals</a>
-                                <a class="collapse-item" href="popovers.html">Popovers</a>
-                                <a class="collapse-item" href="progress-bar.html">Progress Bars</a>
+                                <router-link class="collapse-item" to="/store-employee">Add Employee</router-link>
+                                <router-link class="collapse-item" to="buttons.html">All Employee</router-link>
+                            </div>
+                        </div>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseSuppplierBootstrap" aria-expanded="true" aria-controls="collapseBootstrap">
+                            <i class="far fa-fw fa-window-maximize"></i>
+                            <span>Suppliers</span>
+                        </a>
+                        <div id="collapseSuppplierBootstrap" class="collapse" aria-labelledby="headingBootstrap" data-parent="#accordionSidebar">
+                            <div class="bg-white py-2 collapse-inner rounded">
+                                <a class="collapse-item" href="alerts.html">Add Supplier</a>
+                                <a class="collapse-item" href="buttons.html">All Suppliers</a>
                             </div>
                         </div>
                     </li>
